dc44de47905a4f70593ce1307519b3638eb61ec3
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-08-03 09:33:54 +0600

    post api

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -5,3 +5,19 @@ export function GET() {
   const data = users;
   return NextResponse.json(data, { status: 200 });
 }
+
+export async function POST(request) {
+  const payload = await request.json();
+  if (!payload.name || !payload.age || !payload.email) {
+    return NextResponse.json(
+      { result: "Required data not found" },
+      { success: "false" },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json(
+    { result: "Data inserted success" },
+    { success: true },
+    { status: 201 }
+  );
+}
